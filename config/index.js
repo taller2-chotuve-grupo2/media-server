e876b5608d28635ff3bc56a07d8186d6c512c0e7
juan <juandambra@programming4design.com>
2020-04-26 21:35:52 -0300

    added port config

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ const config = {
         password: process.env.DB_PASSWORD
       },
       environment: process.env.NODE_ENV || "development",
-      port: process.env.NODE_PORT || 3030
+      port: process.env.PORT || 3030
     }
   };
 
