e54c90a27968994b907e271abb2deb07057270af
Juan Pablo Donato <judonato@fi.uba.ar>
2020-05-26 17:54:05 -0300

    add unit and integration tests for resourceService

--- services/resourceService.js
+++ services/resourceService.js
@@ -1,9 +1,19 @@
-async function upload (videoData, repository) {
-  var aux = await repository.getOneByTitle(videoData)
+async function upload (resourceData, repository) {
+  var aux = await repository.getOneByTitle(resourceData.title)
+  if (aux) {
+    return false
+  }
+  aux = await repository.createResource(resourceData)
   if (!aux) {
     return false
   }
   return true
 }
 
+async function getById (id, repository) {
+  var resource = await repository.getOneById(id)
+  return resource
+}
+
 exports.upload = upload
+exports.getById = getById
--- test/resourceService.test.js
+++ test/resourceService.test.js
@@ -2,21 +2,70 @@ const expect = require('chai').expect
 const resourceService = require('../services/resourceService.js')
 const resourceRepositoryMock = require('../repositories/mocks/resourceRepositoryMock.js')
 const resourceRepositoryFailingMock = require('../repositories/mocks/resourceRepositoryFailingMock.js')
+const resourceRepository = require('../repositories/resourceRepository.js')
+const dataCreator = require('../dataCreator/dataCreator.js')
 
 describe('resourceService', () => {
-  it('returns true if everything is ok', async function () {
-    const data = {
-      id: '0800-R1CH4RD-555'
-    }
-    const result = await resourceService.upload(data, resourceRepositoryMock)
-    return expect(result).to.be.true
+  beforeEach('Generate Test Data', async () => {
+    await dataCreator.seedAll()
   })
 
-  it('returns false if video upload fails', async function () {
-    const data = {
-      name: 'Bad video'
-    }
-    const result = await resourceService.upload(data, resourceRepositoryFailingMock)
-    return expect(result).to.be.false
+  afterEach('clean data', async () => {
+    await dataCreator.cleanTables()
+  })
+
+  context('Upload', () => {
+    it('returns true if upload is ok', async function () {
+      const data = {
+        id: '0800-R1CH4RD-555'
+      }
+      const result = await resourceService.upload(data, resourceRepositoryMock)
+      return expect(result).to.be.true
+    })
+
+    it('returns false if resource upload fails', async function () {
+      const data = {
+        name: 'Bad video'
+      }
+      const result = await resourceService.upload(data, resourceRepositoryFailingMock)
+      return expect(result).to.be.false
+    })
+  })
+
+  context('getById', () => {
+    it('should get a resource by id', async function () {
+      const result = await resourceService.getById('1', resourceRepositoryMock)
+      return expect(result.id).to.be.eql('1')
+    })
+
+    it('should return null if resource not found', async function () {
+      const result = await resourceService.getById('bad_id', resourceRepositoryFailingMock)
+      return expect(result).to.be.null
+    })
+  })
+
+  context('Integration tests', () => {
+    context('With resourceRepository', () => {
+      it('should get a resource by id', async function () {
+        const result = await resourceService.getById('1', resourceRepository)
+        return expect(result.id).to.be.eql('1')
+      })
+
+      it('returns true if upload is ok', async function () {
+        const data = {
+          id: '5',
+          name: 'My first name',
+          path: 'www.richard.com',
+          size: '1MB',
+          owner: 'RICHARDINHO',
+          title: 'REPEATED TITLE',
+          description: 'ASDASD',
+          location: 'Argentina',
+          visibility: 'Public'
+        }
+        const result = await resourceService.upload(data, resourceRepository)
+        return expect(result).to.be.true
+      })
+    })
   })
 })
