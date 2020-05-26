9fbc1f1685fa470d510628ccb6346a37ac544bf1
Juan Pablo Donato <judonato@fi.uba.ar>
2020-05-26 17:53:41 -0300

    dataCreator added

---
+++ dataCreator/dataCreator.js
@@ -0,0 +1,31 @@
+const Resource = require('../models').Resource
+const sequelize = require('../models').sequelize
+
+const resourceData = {
+  id: '1',
+  name: 'My first name',
+  path: 'www.richard.com',
+  size: '1MB',
+  owner: 'RICHARDINHO',
+  title: 'RICHARD VIDEO',
+  description: 'ASDASD',
+  location: 'Argentina',
+  visibility: 'Public'
+}
+
+async function seedAll () {
+  await seedResources()
+}
+
+async function seedResources () {
+  await Resource.upsert(resourceData)
+}
+
+async function cleanTables () {
+  await Object.values(sequelize.models).map(function (model) {
+    return model.destroy({ truncate: true })
+  })
+}
+
+exports.seedAll = seedAll
+exports.cleanTables = cleanTables
