e4c7121f4cd60aef1d25bbabc8e6e9444967ccbc
Juan Pablo Donato <judonato@fi.uba.ar>
2020-06-28 11:02:07 -0300

    fix lint

--- migrations/20200628133412-add_thumbnails_to_resources.js
+++ migrations/20200628133412-add_thumbnails_to_resources.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Resources',
-    'Thumbnail',
-    {type: Sequelize.STRING}
-    );
+      'Thumbnail',
+      { type: Sequelize.STRING }
+    )
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.removeColumn(
       'Resources',
       'Thumbnail'
-    );
+    )
   }
-};
+}
